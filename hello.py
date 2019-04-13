db5f6cb1d211c23dcc6ed68b43352079825c3843
BARarch <anthonyquivers@gmail.com>
2019-04-13 14:28:42 -0400

    SHEBANG the input pipe works very specific process tho

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+print(input())
+print(input())
+print("Helo worldz")
+
+## WINNER WINNER CHICKEN DINNER
+## We are in buisness
+## The input pipe has been derived.  -> $ INPUT_FILE | PYTHON_FILE.py 
+##
+## given:
+## 1: The shebang line "#!/usr/bin/env python3" is at the top
+## 2: 
