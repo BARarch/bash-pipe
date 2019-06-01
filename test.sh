a6a60c6f40f74cf5a8fa89a0f252c1580844f68e
BARarch <anthonyquivers@gmail.com>
2019-06-01 16:57:56 -0400

    with special output file

--- test.sh
+++ test.sh
@@ -7,17 +7,24 @@ else
   TITLE="$1"
 fi
 
+OUTS="test-run-output.txt"
+export OUTPUT_PATH="$OUTS"
+
 if [ -z "$2" ]; then
   FILES=$TITLE-cases/*
   for f in $FILES
   do
+    echo "" > "$OUTS"
     echo ""
     echo "Testing $TITLE with $f..."
     $TITLE.py < $f
+    cat "$OUTS"
   done
 else
   echo ""
+  echo "" > "$OUTS"
   SINGLECASE="$TITLE-cases/$2"
   echo "Testing $TITLE with $2..."
   $TITLE.py < $SINGLECASE
+  cat "$OUTS"
 fi
