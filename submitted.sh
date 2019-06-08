a429129fd171c2ac3e6374295020293fc14fb782
BARarch <anthonyquivers@gmail.com>
2019-06-08 13:50:13 -0400

    Submitted on 19-06-08 at 13:50:13

---
+++ submitted.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Which one have you submitted"
+  read TITLE
+else
+  TITLE="$1"
+fi
+
+git add .
+
+d=date
+fd=+'%y-%m-%d'
+td=$($d $fd)
+ft=+'%T'
+ts=$($d $ft)
+
+if [ -z "$2" ]; then
+  git commit -a -m "Submitted on $td at $ts"
+else
+  MSG = $2
+  git commit -a -m "Submitted on $td at $ts $MSG"
+fi  
+git push origin master
+
+git status
